f2fd8865e561d3e9422ace8169d8b0ec08e4a640
dannebernhardt199610 <danielbernhardt1996@outlook.com>
2020-09-05 22:32:37 +0200

    added some more test methods

--- src/test/java/javaEEHelloWorld/FortuneTest.java
+++ src/test/java/javaEEHelloWorld/FortuneTest.java
@@ -8,9 +8,35 @@ public class FortuneTest {
 
     Fortune fortune = new Fortune();
 
+
     @Test
     public void testResult(){
         fortune.setResult("");
         assertEquals(fortune.getResult(),"");
     }
+
+    @Test
+    public void testAdd(){
+        fortune.setColor("blue");
+        fortune.setName("danne");
+        fortune.setFood("tacos");
+        assertEquals(fortune.getColor(), "blue");
+        assertEquals(fortune.getName(), "danne");
+        assertEquals(fortune.getFood(), "tacos");
+    }
+
+    @Test
+    public void testMsg(){
+        fortune.setMsg("Invalid name");
+        assertEquals(fortune.getMsg(), "Invalid name");
+        fortune.setMsg("");
+        assertEquals(fortune.getMsg(), "");
+    }
+
+    @Test
+    public void testAnything(){
+    }
+
+
+
 }
