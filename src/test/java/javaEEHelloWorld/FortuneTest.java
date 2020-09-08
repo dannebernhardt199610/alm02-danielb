b0c4809e529adc875ee7aa13ac4c2ca1ee351a15
dannebernhardt199610 <danielbernhardt1996@outlook.com>
2020-09-08 14:28:22 +0200

    added docker

--- src/test/java/javaEEHelloWorld/FortuneTest.java
+++ src/test/java/javaEEHelloWorld/FortuneTest.java
@@ -2,17 +2,22 @@ package javaEEHelloWorld;
 
 import org.junit.Test;
 
+import java.util.logging.Logger;
+
 import static org.junit.Assert.assertEquals;
 
 public class FortuneTest {
 
+    private final static Logger LOGGER = Logger.getLogger(Fortune.class.getName());
     Fortune fortune = new Fortune();
+
 
 
     @Test
     public void testResult(){
-        fortune.setResult("");
-        assertEquals(fortune.getResult(),"");
+        fortune.setResult("hej");
+        assertEquals(fortune.getResult(),"hej");
+        LOGGER.info(fortune.getResult());
     }
 
     @Test
@@ -21,20 +26,39 @@ public class FortuneTest {
         fortune.setName("danne");
         fortune.setFood("tacos");
         assertEquals(fortune.getColor(), "blue");
+        LOGGER.info(fortune.getColor());
         assertEquals(fortune.getName(), "danne");
+        LOGGER.info(fortune.getName());
         assertEquals(fortune.getFood(), "tacos");
+        LOGGER.info(fortune.getFood());
     }
 
     @Test
     public void testMsg(){
         fortune.setMsg("Invalid name");
         assertEquals(fortune.getMsg(), "Invalid name");
+        LOGGER.info(fortune.getMsg());
         fortune.setMsg("");
         assertEquals(fortune.getMsg(), "");
+        LOGGER.info(fortune.getMsg());
     }
 
     @Test
-    public void testAnything(){
+    public void testCheck(){
+        fortune.setName("danne");
+        fortune.setFood("tacos");
+        fortune.setColor("green");
+        fortune.check();
+    }
+
+    @Test
+    public void testTellFortune(){
+        fortune.setName("daniel");
+        fortune.setFood("tacos");
+        fortune.setColor("green");
+        fortune.tellFortune();
+        assertEquals(fortune.getResult(), "Say hello to others. You will have a happier day.");
+        LOGGER.info(fortune.getResult());
     }
 
 
